d9c3c78403fec3bcd864cc722d8d2bcc39213fb6
Leonardo Maciel <leonardomacielj@gmail.com>
2025-08-05 09:54:13 -0300

    chore: add drizzle setup with initial schema; add seed script

---
+++ src/db/schema.ts
@@ -0,0 +1,67 @@
+import { param, relations } from "drizzle-orm";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+export const UserTable = pgTable("user", {
+  id: uuid().primaryKey(),
+  name: text().notNull(),
+});
+
+export const categoryTable = pgTable("category", {
+  id: uuid().primaryKey(),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  createdAt: timestamp().notNull().defaultNow(),
+});
+
+export const categoryRelations = relations(categoryTable, ({ many }) => ({
+  products: many(productTable),
+}));
+
+export const productTable = pgTable("product", {
+  id: uuid().primaryKey(),
+  categoryId: uuid("category_id")
+    .notNull()
+    .references(() => categoryTable.id),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  description: text().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const productRelations = relations(productTable, ({ one, many }) => ({
+  category: one(categoryTable, {
+    fields: [productTable.categoryId],
+    references: [categoryTable.id],
+  }),
+  variants: many(productVariantTable),
+}));
+
+export const productVariantTable = pgTable("product_variant", {
+  id: uuid().primaryKey(),
+  productId: uuid("product_id")
+    .notNull()
+    .references(() => productTable.id),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  color: text().notNull(),
+  priceInCents: integer("price_in_cents").notNull(),
+  imageUrl: text("img_url").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const productVariantRelations = relations(
+  productVariantTable,
+  ({ one }) => ({
+    product: one(productTable, {
+      fields: [productVariantTable.id],
+      references: [productTable.id],
+    }),
+  }),
+);
