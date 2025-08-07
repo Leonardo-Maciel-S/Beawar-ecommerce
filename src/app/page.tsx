9ff37260177c36b876e0e5f189bcc4bd77dd19ff
Leonardo Maciel <leonardomacielj@gmail.com>
2025-08-07 09:58:27 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelect from "@/components/common/category-selector";
 import ProductsList from "@/components/common/products-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
     with: {
       variants: true,
     },
+  });
+
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+
+    orderBy: [desc(productTable.createdAt)],
   });
 
   const categories = await db.query.categoryTable.findMany({});
@@ -43,6 +53,8 @@ const Home = async () => {
             className="h-auto w-full"
           />
         </div>
+
+        <ProductsList products={newlyCreatedProducts} title="Novos produtos" />
       </div>
     </>
   );
---
+++ src/components/common/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-accent mt-6 w-full gap-1 p-8">
+      <p className="text-xs font-medium">© 2025 Copyright BEWEAR</p>
+      <p className="text-muted-foreground text-xs font-medium">
+        Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
